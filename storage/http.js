6f385d3127dd2435153f580fb8770a4aa48ad8bc
sun <sunqch@neusoft.com>
2019-05-31 09:19:05 +0800

    db

--- storage/http.js
+++ storage/http.js
@@ -2,7 +2,6 @@ const config_info = require('./config.js');
 const {
     ipcRenderer
 } = require('electron')
-
 window.$ = window.jQuery = require('jquery');
 $(function () {
 
@@ -12,26 +11,18 @@ $(function () {
      *
      */
     $.ajax({
-
         type: "POST",
-
         url: config_info.online_url,
-
         data: {
             'ip': config_info.localhost
         },
-
         dataType: "json",
-        contentType: "application/json;charset=UTF-8",
         success: function (data) {
-
             console.log(data)
-
         },
         error:function (data) {
-            console.log("dfd")
+            console.log(data)
         }
-
     });
 
 
@@ -39,50 +30,36 @@ $(function () {
      *
      */
     $.ajax({
-
         type: "GET",
-
         url: config_info.get_online_users_url,
         success: function (data) {
             var ipList = data.data;
             for(var i in ipList){
                 if(ipList[i].ip !=  config_info.localhost){
-                    var a = '<a class="panel-block" title="双击" ondblclick="mixInput(this)" data="' + ipList[i].ip +
+                    var a = '<span class="panel-block" title="双击" ondblclick="mixInput(this)" data="' + ipList[i].ip +
                         '"><span class="panel-icon">' +
-                        '<i class="fa fa-desktop"></i></span>' +
-                        ipList[i].ip + '</a>';
+                        '<i class="fa fa-desktop"></i></span><a>' +
+                        ipList[i].nickName+" </a>:"+ipList[i].ip + '</span>';
                     $("#onlineIpList").append(a);
                 }
-
             }
             console.log(data)
-
         }
-
     });
 
 });
 
 ipcRenderer.on('offline', (event, data) => {
-
 $.ajax({
-
     type: "POST",
-
     url: config_info.offline_url,
-
     data: {
         'ip': config_info.localhost
     },
-
     dataType: "json",
-
     success: function (data) {
-
         console.log(data)
-
     }
-
 });
 });
 
